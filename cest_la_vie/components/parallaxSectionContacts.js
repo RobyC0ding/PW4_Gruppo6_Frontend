8d81a31522ee48d123fa8a2df5d65b1dd0a7aadf
Roberto <roberto.zuccaro@itsincom.it>
2024-11-07 11:46:56 +0100

    fix login

--- cest_la_vie/components/parallaxSectionContacts.js
+++ cest_la_vie/components/parallaxSectionContacts.js
@@ -1,21 +1,10 @@
 'use client'
-import {useEffect, useState} from "react";
 import styles from "./parallaxSectionContacts.module.css";
 
 export default function ParallaxSectionContacts() {
-    const [offsetY, setOffsetY] = useState(0);
-
-    const handleScroll = () => setOffsetY(window.scrollY);
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
-
-
     return (
         <div className={styles.informationContainer}>
-            <div className={styles.backgroundImage}></div>
+            <div className={styles.backgroundImage}></div> {/* Background gestito via CSS */}
             <h3 className={styles.informationTitle}>Per richiedere informazioni</h3>
             <p>
                 <strong>Email:</strong> <a href="mailto:info@pasticceriacestlavie.it">info@pasticceriacestlavie.it</a><br /><br />
